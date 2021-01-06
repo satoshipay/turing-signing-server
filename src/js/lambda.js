948ff2df89270b65c0b62bdc5af26504298c93b8
Marcel Ebert <mail@marcel-ebert.de>
2021-01-06 12:08:16 +0100

    Add secrets to lambda dev object Might be unnecessary but better safe than sorry

--- src/js/lambda.js
+++ src/js/lambda.js
@@ -1,16 +1,19 @@
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
 
-import { isDev } from './utils'
+import { isDev } from "./utils";
 
 let options = {
-  apiVersion: '2015-03-31',
-  region: 'us-east-1',
-  sslEnabled: true
-}
+  apiVersion: "2015-03-31",
+  region: "us-east-1",
+  sslEnabled: true,
+};
 
-if (isDev) options = {
-  ...options,
-  endpoint: `http://localhost:4566`
-}
+if (isDev)
+  options = {
+    ...options,
+    endpoint: `http://localhost:4566`,
+    accessKeyId: "foo",
+    secretAccessKey: "bar",
+  };
 
-export default new AWS.Lambda(options)
+export default new AWS.Lambda(options);
